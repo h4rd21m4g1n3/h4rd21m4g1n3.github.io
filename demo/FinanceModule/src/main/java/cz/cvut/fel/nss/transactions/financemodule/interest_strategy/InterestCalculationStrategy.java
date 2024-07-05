4d031dd2656df232bfe1484d83ef309f1bdca2ce
Vladyslav Hordiienko <hordivla@fel.cvut.cz>
2024-07-05 12:39:45 +0000

    Delete demo

--- demo/FinanceModule/src/main/java/cz/cvut/fel/nss/transactions/financemodule/interest_strategy/InterestCalculationStrategy.java
+++
@@ -1,5 +0,0 @@
-package cz.cvut.fel.nss.transactions.financemodule.interest_strategy;
-
-public interface InterestCalculationStrategy {
-    float calculateInterest(float amount, long daysBetween, int interestRate);
-}
--- demo/README.md
+++
@@ -1,4 +0,0 @@
-How to install app
--
-Git clone application, open directory, cd to front_module directory,
-run npm install, run npm start
